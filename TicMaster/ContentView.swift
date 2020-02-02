f45e408162ed60f8bf64092b764cf8019eff8de7
Charles Zeng <judiciousness@gmail.com>
2020-02-02 15:00:21 -0500

    Initial Commit

---
+++ TicMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicMaster
+//
+//  Created by Chao Zeng on 2/2/20.
+//  Copyright © 2020 livingston. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
