22cc2cb092666cfa89c9d0e4f46071787387cb26
Charles Zeng <judiciousness@gmail.com>
2020-02-09 22:59:19 -0500

    add camera view

--- TicMaster/ContentView.swift
+++ TicMaster/ContentView.swift
@@ -9,74 +9,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var imageA: Image? = Image("ticket")
+    @State var showCaptureImageView: Bool = false
+    
     var body: some View {
         ZStack
-        {
-            Color.white
-
-
-        VStack(alignment: .leading) {
-            HStack {                Text("Livingston").font(.headline).foregroundColor(Color.black)
-                Spacer()
-            }
-            HStack {
-                Button(action: {
-                    self.touchAction("A")
-
-                }) {
-                    Image("ticket")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+            {
+                Color.white
+                VStack(alignment: .leading) {
+                    HStack {                Text("Livingston").font(.headline).foregroundColor(Color.black)
+                        Spacer()
+                    }
+                    HStack {
+                        Button(action: {
+                            self.touchAction("A")
+                            
+                        }) {
+                            imageA?
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        
+                        Spacer()
+                        Button(action: {
+                            self.touchAction("B")
+                            
+                        }) {
+                            Image("add")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        
+                    }
+                    HStack{
+                        Text("Arena").font(.headline).foregroundColor(Color.black)
+                        Spacer()
+                    }
+                    HStack {
+                        Button(action: {
+                            self.touchAction("C")
+                            
+                        }) {
+                            Image("ticket")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        
+                        Spacer()
+                        Button(action: {
+                            self.touchAction("D")
+                            
+                        }) {
+                            Image("no-ticket")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        
+                    }
+                    Spacer()
+                }.padding()
+//                .sheet(isPresented: true, onDismiss: nil, content: ImagePicker(image: self.$image))
+                if (showCaptureImageView) {
+                  CaptureImageView(isShown: $showCaptureImageView, image: $imageA)
                 }
-                    
-                Spacer()
-                Button(action: {
-                  self.touchAction("B")
-
-                }) {
-                    Image("add")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-                    
-            }
-            HStack{
-                Text("Arena").font(.headline).foregroundColor(Color.black)
-                Spacer()
-            }
-            HStack {
-                Button(action: {
-                  self.touchAction("C")
-
-                }) {
-                    Image("ticket")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-                    
-                Spacer()
-                Button(action: {
-                  self.touchAction("D")
-
-                }) {
-                    Image("no-ticket")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-                    
-            }
-            Spacer()
-            }.padding()
-            
-        
         }
     }
     private func touchAction(_ btn: String) {
-      print(btn)
+        print(btn)
+        //        ImagePicker(image: self.$image)
+        self.showCaptureImageView.toggle()
     }
 }
 
@@ -86,4 +92,31 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct CaptureImageView {
+  
+  /// MARK: - Properties
+  @Binding var isShown: Bool
+  @Binding var image: Image?
+  
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(isShown: $isShown, image: $image)
+  }
+}
+
+extension CaptureImageView: UIViewControllerRepresentable {
+  func makeUIViewController(context: UIViewControllerRepresentableContext<CaptureImageView>) -> UIImagePickerController {
+    let picker = UIImagePickerController()
+    picker.delegate = context.coordinator
+    /// Default is images gallery. Un-comment the next line of code if you would like to test camera
+//    picker.sourceType = .camera
+    return picker
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController,
+                              context: UIViewControllerRepresentableContext<CaptureImageView>) {
+    
+  }
 }
---
+++ TicMaster/Coordinator.swift
@@ -0,0 +1,32 @@
+//
+//  Coordinator.swift
+//  TicMaster
+//
+//  Created by Chao Zeng on 2/9/20.
+//  Copyright © 2020 livingston. All rights reserved.
+//
+
+import SwiftUI
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @Binding var isCoordinatorShown: Bool
+    @Binding var imageInCoordinator: Image?
+    
+    init(isShown: Binding<Bool>, image: Binding<Image?>) {
+        _isCoordinatorShown = isShown
+        _imageInCoordinator = image
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let unwrapImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        imageInCoordinator = Image(uiImage: unwrapImage)
+        isCoordinatorShown = false
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isCoordinatorShown = false
+    }
+}
+
